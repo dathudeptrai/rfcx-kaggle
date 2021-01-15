4d165aafd8bb0500c5408a2ccca55ab12222b9e3
dathudeptrai <nguyenquananhminh@gmail.com>
2021-01-16 00:10:53 +0700

    👌 Remove bootstrap y_true for lrap loss.

--- losses.py
+++ losses.py
@@ -22,16 +22,10 @@ def approx_rank(logits):
     return tf.reduce_sum(input_tensor=pairs, axis=-1) + 0.5
 
 
-def label_ranking_loss_tf(y_true, y_pred, is_fp=False, is_train=False, beta=0.7):
+def label_ranking_loss_tf(y_true, y_pred, is_fp=False):
     n_samples = tf.shape(y_true)[0]
     indptr = tf.py_function(compute_indptr, [y_true], Tout=tf.int32)
     y_true = tf.cast(y_true, dtype=y_pred.dtype)
-
-    if is_train:
-        # update soft y_true
-        y_pred_soft = tf.stop_gradient(tf.nn.sigmoid(y_pred))  # [B, 24]
-        y_true_update = beta * y_true + (1.0 - beta) * y_pred_soft
-        y_true = y_true_update
 
     y_true_indices = tf.py_function(compute_y_true_indices, [y_true], Tout=tf.int32)
 
